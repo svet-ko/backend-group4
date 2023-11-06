1a8aa595dfcb0b27612d49ce5de6d0f87f5d3673
Ieva Vyliaudaite <ievavyliaudaite@Integrifys-MacBook-Pro.local>
2023-11-06 12:48:22 +0200

    adding changes

--- types/user.ts
+++ types/user.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { userDataSchema } from "../middlewares/userValidate.js";
+import { userDataSchema } from "../schemas/userSchema.js";
 import { loginRequestSchema } from "../middlewares/validateLoginRequest.js";
 
 export type UserDTO = z.infer<typeof userDataSchema>
