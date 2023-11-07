82bcedf1191b04550ce663623e925bf4d3417046
Md Touhidul islam <96823033+mdtouhidulislam136@users.noreply.github.com>
2023-11-07 17:40:55 +0200

    DB connected

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
-import itemsRoute from "./routes/itemsRoute.js";
-import productsRoute from "./routes/productsRoute.js";
 import { loggingMiddleware } from "./middlewares/logging.js";
 import { apiErrorHandler } from "./middlewares/error.js";
 import { routeNotFound } from "./middlewares/routeNotFound.js";
+import authRoute from "./routes/authRoute.js";
+import categoriesRoute from "./routes/categoriesRoute.js";
+import itemsRoute from "./routes/itemsRoute.js";
+import productsRoute from "./routes/productsRoute.js";
+import usersRoute from "./routes/usersRoute.js";
 const PORT = 8080;
 const app = express();
 app.use(express.json());
@@ -15,8 +18,12 @@ mongoose.connect(mongoURL).then(() => console.log("Connected!"));
 app.get("/hello", loggingMiddleware, (_, res) => {
     res.json({ msg: "hello, from Express.js!" });
 });
-app.use("/api/v1/items", itemsRoute);
+app.use(loggingMiddleware);
+app.use("/api/v1/categories", categoriesRoute);
+app.use("/api/v1/users", usersRoute);
+app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/products", productsRoute);
+app.use("/api/v1/items", itemsRoute);
 app.use(apiErrorHandler);
 app.use(routeNotFound);
 app.listen(PORT, () => {
