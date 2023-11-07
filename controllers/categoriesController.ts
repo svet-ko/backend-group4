6c9f23893122a5e1ce3f851d0f9aa5f031df8f26
Md Touhidul islam <96823033+mdtouhidulislam136@users.noreply.github.com>
2023-11-07 19:39:05 +0200

    Database connection and CRUD operation

--- controllers/categoriesController.ts
+++ controllers/categoriesController.ts
@@ -1,12 +1,13 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
 
-import CategoriesService from "../services/categoriesService.js"
-import { ApiError } from "../errors/ApiError.js"
-import { ObjectId } from "mongoose"
+import CategoriesService from "../services/categoriesService.js";
+import { ApiError } from "../errors/ApiError.js";
+import { ObjectId } from "mongoose";
+import categoriesService from "../services/categoriesService.js";
 
 export async function getAllCategories(_: Request, res: Response) {
-  const categories = await CategoriesService.getAll()
-  res.json({ categories })
+  const categories = await CategoriesService.getAll();
+  res.json({ categories });
 }
 
 export async function getOneCategory(
@@ -14,28 +15,68 @@ export async function getOneCategory(
   res: Response,
   next: NextFunction
 ) {
-  const categoryId = req.params.categoryId
-  const category = await CategoriesService.getOne(categoryId)
+  const categoryId = req.params.categoryId;
+  const category = await CategoriesService.getOne(categoryId);
 
   if (!category) {
-    next(ApiError.resourceNotFound("Category not found."))
-    return
+    next(ApiError.resourceNotFound("Category not found."));
+    return;
   }
-  res.json({ category })
+  res.json({ category });
 }
 
 export async function createCategory(req: Request, res: Response) {
-  const newCategory = req.body
-  const category = await CategoriesService.createCategory(newCategory)
-  res.status(201).json({ category })
+  const newCategory = req.body;
+  const category = await CategoriesService.createCategory(newCategory);
+  res.status(201).json({ category });
+}
+
+export async function updateCategory(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const categoryId = req.params.categoryId;
+  const categoryData = req.body;
+  const category = CategoriesService.getOne(categoryId);
+
+  if (!category) {
+    next(ApiError.resourceNotFound("Category not found"));
+    return;
+  }
+
+  const result = await CategoriesService.updateCategory(
+    categoryId,
+    categoryData
+  );
+  res.status(200).json({ result });
+}
+
+export async function deleteCategory(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const categoryId = req.params.categoryId;
+  const category = CategoriesService.getOne(categoryId);
+
+  if (category === null) {
+     next(ApiError.resourceNotFound("Category does not exist"));
+     return;
+  }
+
+  CategoriesService.deleteCategory(categoryId);
+  res.status(200).json({category})
+
 }
 
 export default {
   getOneCategory,
   getAllCategories,
   createCategory,
-}
-
+  updateCategory,
+  deleteCategory,
+};
 
 /*import { NextFunction, Request, Response } from "express";
 import CategoriesService from "../services/categoriesService.js";
--- services/categoriesService.ts
+++ services/categoriesService.ts
@@ -1,29 +1,51 @@
-import mongoose, { ObjectId } from "mongoose"
-import CategoryRepo from "../models/Category.js"
-import { Category } from "../types/category.js"
+import mongoose, { ObjectId } from "mongoose";
+import CategoryRepo from "../models/Category.js";
+import { Category } from "../types/category.js";
 
 async function getAll() {
-  const categories = await CategoryRepo.find().exec()
-  return categories
+  const categories = await CategoryRepo.find().exec();
+  return categories;
 }
 
 async function getOne(categoryId: string) {
-  const id = new mongoose.Types.ObjectId(categoryId)
-  const category = await CategoryRepo.findById(id)
-  return category
+  const id = new mongoose.Types.ObjectId(categoryId);
+  const category = await CategoryRepo.findById(id);
+  return category;
 }
 
 async function createCategory(category: Category) {
-  const newCategory = new CategoryRepo(category)
-  return await newCategory.save()
+  const newCategory = new CategoryRepo(category);
+  return await newCategory.save();
+}
+
+async function updateCategory(
+  categoryId: string,
+  updateCategory: Partial<Category>
+) {
+  const id = new mongoose.Types.ObjectId(categoryId);
+  const result = await CategoryRepo.updateOne(
+    { _id: id },
+    { $set: updateCategory }
+  );
+  if (!result) {
+    return null;
+  }
+
+  return await CategoryRepo.findById(id);
+}
+
+export async function deleteCategory(categoryId: string) {
+  const id = new mongoose.Types.ObjectId(categoryId);
+  return await CategoryRepo.findByIdAndDelete(categoryId);
 }
 
 export default {
   getOne,
   getAll,
-  createCategory
-}
-
+  createCategory,
+  updateCategory,
+  deleteCategory,
+};
 
 /*import { CategoryRepo } from "../models/Category.js";
 import { Category } from "../types/category.js";
