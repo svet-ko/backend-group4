ce09ea66fee9fb3e158c074d705dccf7cde13837
Md Touhidul islam <96823033+mdtouhidulislam136@users.noreply.github.com>
2023-11-08 20:54:47 +0200

    Revert "Removed dist folder from tracking"
    This reverts commit 82eedcf29be2cd2f0f9195060b6e790c8b3b66bf.

---
+++ dist/models/Product.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+const ProductSchema = new Schema({
+    id: ObjectId,
+    name: String,
+});
+export default mongoose.model("Product", ProductSchema);
---
+++ dist/schemas/productSchema.js
@@ -0,0 +1,9 @@
+import { z } from "zod";
+export const productSchema = z.object({
+    name: z.string({
+        required_error: "Name is required",
+    }),
+});
+export const requestSchema = z.object({
+    body: productSchema,
+});
