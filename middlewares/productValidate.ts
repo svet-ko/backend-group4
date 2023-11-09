e8557a0b598a39196d2ac61b995a5261e8fb3ed5
svetlana_korneeva <svet-ko@list.ru>
2023-11-09 18:14:52 +0200

    feat: category-product connect without err handler

--- middlewares/productValidate.ts
+++ middlewares/productValidate.ts
@@ -1,25 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import { z } from "zod"
-
-export const productSchema = z.object({
-  body: z.object({
-    title: z.string({
-      required_error: "Title is required",
-    }),
-    price: z
-      .number({
-        required_error: "Price is required",
-      })
-      .positive(),
-    description: z.string({
-      required_error: "Description is required",
-    }),
-    categoryId: z.number({
-      required_error: "CategoryId is required",
-    }),
-    image: z.string().optional(),
-  })
-})
+import { productToCreateSchema } from "../schemas/productSchema.js"
 
 export async function validateProduct(
   req: Request,
@@ -27,7 +8,7 @@ export async function validateProduct(
   next: NextFunction
 ) {
   try {
-    await productSchema.parseAsync({
+    await productToCreateSchema.parseAsync({
       body: req.body,
       query: req.query,
       params: req.params,
