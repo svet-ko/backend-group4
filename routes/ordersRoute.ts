2c8deeedf8b6d644fe518b5de51c3e37aa250ab2
Ieva Vyliaudaite <ievavyliaudaite@Integrifys-MacBook-Pro.local>
2023-11-09 19:50:52 +0200

    saving progress

--- routes/ordersRoute.ts
+++ routes/ordersRoute.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 // TO DO: auth middleware to authorize get all access
 router.get("/", OrdersController.getAllOrders);
 router.get("/:userId", OrdersController.getOrderByUserId);
+router.post("/checkout", OrdersController.createOrder);
 
 
 export default router;
