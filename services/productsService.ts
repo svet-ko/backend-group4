e34b68ffaa5720a2d4fcaee6582d864ea78134e5
svetlana_korneeva <svet-ko@list.ru>
2023-11-10 19:10:45 +0200

    feat: added getTotalPrice for product service

--- services/productsService.ts
+++ services/productsService.ts
@@ -1,49 +1,59 @@
-import mongoose, { ObjectId } from "mongoose"
+import mongoose, { ObjectId } from "mongoose";
 
-import ProductRepo from "../models/Product.js"
-import { Product, ProductToCreate } from "../types/products.js"
+import ProductRepo from "../models/Product.js";
+import { Product, ProductToCreate } from "../types/products.js";
 import CategoryRepo from "../models/Category.js";
 import { Category } from "../types/category.js";
 
 async function findAll() {
-  const products = await ProductRepo.find().exec()
-  return products
+  const products = await ProductRepo.find().populate("category").exec();
+  return products;
 }
 
 async function findOne(productId: string) {
-  const id = new mongoose.Types.ObjectId(productId)
-  const product = await ProductRepo.findById(id)
+  const id = new mongoose.Types.ObjectId(productId);
+  const product = await ProductRepo.findById(id);
 
-  return product
+  return product;
 }
 
 async function createOne(product: ProductToCreate) {
   const category: Category | null = await CategoryRepo.findOne({
-    _id: product.categoryId
-  })
+    _id: product.categoryId,
+  });
   if (category) {
     delete product.categoryId;
     product.category = category;
     const newProduct = new ProductRepo(product);
     console.log(newProduct);
-    return await newProduct.save()
+    return await newProduct.save();
   }
   return false;
 }
 
-async function updateOne(productId: string, updatesForProduct: Partial<ProductToCreate>) {
+async function updateOne(
+  productId: string,
+  updatesForProduct: Partial<ProductToCreate>
+) {
   const id = new mongoose.Types.ObjectId(productId);
 
-  const category: Category | null = await CategoryRepo.findOne({
-    _id: updatesForProduct.categoryId
-  })
+  if (!!updatesForProduct.categoryId) {
+    const category: Category | null = await CategoryRepo.findOne({
+      _id: updatesForProduct.categoryId,
+    });
 
-  if (category) {
-    delete updatesForProduct.categoryId;
-    updatesForProduct.category = category;
+    if (category) {
+      delete updatesForProduct.categoryId;
+      updatesForProduct.category = category;
+    } else {
+      return;
+    }
   }
 
-  const result = await ProductRepo.updateOne({ _id: id }, { $set: updatesForProduct });
+  const result = await ProductRepo.updateOne(
+    { _id: id },
+    { $set: updatesForProduct }
+  );
 
   if (!result) {
     return null;
@@ -51,7 +61,29 @@ async function updateOne(productId: string, updatesForProduct: Partial<ProductTo
   return await ProductRepo.findById(id);
 }
 
-async function deleteOne(productId: string){
+async function getTotalPrice(
+  orderItemsInput: {
+    _id: mongoose.Types.ObjectId;
+    quantity: number;
+  }[]
+): Promise<number> {
+  const inputIds = orderItemsInput.map((item) => item._id);
+  const products = await ProductRepo.find({ _id: inputIds });
+  const sum = products.reduce((acc, product) => {
+    const inputTargetItem = orderItemsInput.find((item) =>
+      product._id.equals(item._id)
+    );
+    if (product.price) {
+      return !!inputTargetItem
+        ? acc + inputTargetItem.quantity * product.price
+        : acc;
+    }
+    return acc;
+  }, 0);
+  return sum;
+}
+
+async function deleteOne(productId: string) {
   const id = new mongoose.Types.ObjectId(productId);
   return await ProductRepo.findByIdAndDelete(id);
 }
@@ -61,5 +93,6 @@ export default {
   findAll,
   createOne,
   updateOne,
-  deleteOne
-}
+  deleteOne,
+  getTotalPrice,
+};
