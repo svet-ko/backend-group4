46288b3da595d257e59a3841cf84e0e1772d45c5
Md Touhidul islam <96823033+mdtouhidulislam136@users.noreply.github.com>
2023-11-16 10:08:48 +0200

    id removed

--- models/Category.ts
+++ models/Category.ts
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
 
 const CategorySchema = new Schema({
-  id: ObjectId,
   name: {
     type: String,
     required: true,
