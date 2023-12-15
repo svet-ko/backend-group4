244b9fbd3a6afe91d4dfdb67861c6e8900a9b324
svetlana_korneeva <svet-ko@list.ru>
2023-12-15 14:07:37 +0200

    fix: categories service tests

--- test/services/category.test.ts
+++ test/services/category.test.ts
@@ -42,33 +42,33 @@ describe("Category controller", () => {
     expect(newCategory?.name).toEqual("Clothing");
   });
 
-  // it("should return a list of categories", async () => {
-  //   const categories = await categoriesService.getAll();
-  //   expect(categories.length).toEqual(1); // Assuming there is one category created in beforeEach
-  // });
+  it("should return a list of categories", async () => {
+    const categories = await categoriesService.getAll();
+    expect(categories.length).toEqual(1); // Assuming there is one category created in beforeEach
+  });
 
-  // it("should find one category", async () => {
-  //   const foundCategory = await categoriesService.getOne(
-  //     categoryOne._id.toString()
-  //   );
-  //   expect(foundCategory?.name).toEqual("Electronics");
-  //   expect(foundCategory?.description).toEqual(
-  //     "Electronic gadgets and devices"
-  //   );
-  // });
+  it("should find one category", async () => {
+    const foundCategory = await categoriesService.getOne(
+      categoryOne._id.toString()
+    );
+    expect(foundCategory?.name).toEqual("Electronics");
+    expect(foundCategory?.description).toEqual(
+      "Electronic gadgets and devices"
+    );
+  });
 
-  // it("should update category", async () => {
-  //   const updatedCategory = await categoriesService.updateCategory(
-  //     categoryOne._id.toString(),
-  //     { name: "Updated Electronics" }
-  //   );
+  it("should update category", async () => {
+    const updatedCategory = await categoriesService.updateCategory(
+      categoryOne._id.toString(),
+      { name: "Updated Electronics" }
+    );
 
-  //   expect(updatedCategory?.name).toEqual("Updated Electronics");
-  // });
+    expect(updatedCategory?.name).toEqual("Updated Electronics");
+  });
 
-  // it("should delete one category", async () => {
-  //   await categoriesService.deleteCategory(categoryOne._id.toString());
-  //   const categories = await categoriesService.getAll();
-  //   expect(categories.length).toEqual(0);
-  // });
+  it("should delete one category", async () => {
+    await categoriesService.deleteCategory(categoryOne._id.toString());
+    const categories = await categoriesService.getAll();
+    expect(categories.length).toEqual(0);
+  });
 });
