3f31451f2cd7bd7b4f974b6611393eddd48d147a
svetlana_korneeva <svet-ko@list.ru>
2023-12-15 14:15:08 +0200

    fix: user service tests

--- test/services/user.test.ts
+++ test/services/user.test.ts
@@ -31,75 +31,75 @@ describe("User services", () => {
     expect(newUser.name).toEqual("user");
   });
 
-  // it("should return a list", async () => {
+  it("should return a list", async () => {
 
-  //   const newUser = new UsersRepo({
-  //       name: "user",
-  //       email:"user@email.com",
-  //       password:"122345"
-  //   });
+    const newUser = new UsersRepo({
+        name: "user",
+        email:"user@email.com",
+        password:"122345"
+    });
 
-  //   await newUser.save();
-  //   const users = await UserService.getAllUsers();
-  //   expect(users.length).toEqual(1);
-  // });
+    await newUser.save();
+    const users = await UserService.getAllUsers();
+    expect(users.length).toEqual(1);
+  });
 
-  // it("should find user by id", async () => {
+  it("should find user by id", async () => {
 
-  //   const newUser = new UsersRepo({
-  //       name: "user",
-  //       email:"user@email.com",
-  //       password:"122345"
-  //   });
+    const newUser = new UsersRepo({
+        name: "user",
+        email:"user@email.com",
+        password:"122345"
+    });
 
-  //   await newUser.save();
-  //   const user = await UserService.getUserById(newUser._id.toString());
-  //   expect(user).toMatchObject({
-  //       name: "user",
-  //   });
-  // });
+    await newUser.save();
+    const user = await UserService.getUserById(newUser._id.toString());
+    expect(user).toMatchObject({
+        name: "user",
+    });
+  });
 
-  // it("should return token", async () => {
-  //   const token = await UserService.getToken({
-  //       id:"112",
-  //       name: "user",
-  //       email:"user@email.com",
-  //       avatar:"",
-  //       role:"CUSTOMER"
-  //   });
-  //    expect(token).not.toBe(null);
-  // });
+  it("should return token", async () => {
+    const token = await UserService.getToken({
+        id:"112",
+        name: "user",
+        email:"user@email.com",
+        avatar:"",
+        role:"CUSTOMER"
+    });
+     expect(token).not.toBe(null);
+  });
 
-  // it('Should update user', async () => {
-  //   const newUser = await UserService.createUser({
-  //       id:"112",
-  //       name: "user",
-  //       email:"user@email.com",
-  //       password:"122345",
-  //       avatar:"",
-  //       role:"CUSTOMER"
-  //     });
+  it('Should update user', async () => {
+    const newUser = await UserService.createUser({
+        id:"112",
+        name: "user",
+        email:"user@email.com",
+        password:"122345",
+        avatar:"",
+        role:"CUSTOMER"
+      });
 
-  //   const userUpdates = {
-  //       name: "UPDATED NAME"
-  //   }
-  //   const id = newUser._id.toString()
-  //   const updatedUser = await UserService.updateUser(id, userUpdates);
-  //   expect(updatedUser).toMatchObject(userUpdates);
-  // })
-  // it('Should delete user', async () => {
-  //   const newUser = await UserService.createUser({
-  //       id:"112",
-  //       name: "user",
-  //       email:"user@email.com",
-  //       password:"122345",
-  //       avatar:"",
-  //       role:"CUSTOMER"
-  //     });
+    const userUpdates = {
+        name: "UPDATED NAME"
+    }
+    const id = newUser._id.toString()
+    const updatedUser = await UserService.updateUser(id, userUpdates);
+    expect(updatedUser).toMatchObject(userUpdates);
+  })
+  it('Should delete user', async () => {
+    const newUser = await UserService.createUser({
+        id:"112",
+        name: "user",
+        email:"user@email.com",
+        password:"122345",
+        avatar:"",
+        role:"CUSTOMER"
+      });
 
-  //   const id = newUser._id.toString()
-  //   await UserService.deleteUser(id);
-  //   const deleted = await UsersRepo.findById(id)
-  //   expect(deleted).toBe(null);
-  // })
+    const id = newUser._id.toString()
+    await UserService.deleteUser(id);
+    const deleted = await UsersRepo.findById(id)
+    expect(deleted).toBe(null);
+  })
 });
